557ae5ab925b1b80c03bc20227af56da6d3f12a2
Hope Sain <hopessain@gmail.com>
2025-07-11 18:32:04 -1000

    completed the manual attributes basics.

---
+++ 01_manual_classes/02_manual_attributes.py
@@ -0,0 +1,12 @@
+class Person():
+    pass
+
+# Creating an object of the Person class
+p = Person()
+
+# Adding attributes to the class manually
+p.name = "John"
+p.age = 30
+
+# Accessing and printing the attributes
+print(f"Name: {p.name}, Age: {p.age}")
