5b474640ad691710d56a7c424c8f133c367a4fe6
Hope Sain <hopessain@gmail.com>
2025-07-17 08:39:24 -1000

    will get back to this later.

--- 02_dataclasses/07_frozen.py
+++ 02_dataclasses/07_frozen.py
@@ -26,6 +26,29 @@ sain = User("Sain", 25, "sain@email.com") #Attempts to modify this throws an err
 
 print(sain) 
 
+"""
 sain.age = 23 # Attempting to change the age variable and check if it throws an error.
 
 print(sain) #It returns a FrozenInstanceError.
+"""
+
+# The genius you, is currently asking what if I want to freeze a single attribute.
+# Let's say we have a student class with two attributes, student_id (immutable), name (mutable).
+
+@dataclass(frozen=True)
+class StudentID:
+    student_id: str 
+
+@dataclass()
+class Student:
+    id: StudentID
+    name: str
+
+student = Student(StudentID("abc123"), "Hope Sain")
+
+student.id = StudentID("xyz789")
+
+print(student)
+
+# Lol, I thought I may change this but hell na, it is not working as intended. When I find time, will get back to this. 
+# Let me name the commit, will get back to this later.
