f0774ba4431845ffe9432f4a9d092e46325d3ef0
Hope Sain <hopessain@gmail.com>
2025-07-18 08:07:15 -1000

    testing inheritance.

--- 02_dataclasses/08_inheritance.py
+++ 02_dataclasses/08_inheritance.py
@@ -10,5 +10,7 @@ class Employee(Person):
     salary: str
 
 first_employee = Employee("Hope", 23, "150,000")
+second_employee = Employee("Sain", 24, "350,000")
 
 print(first_employee)
+print(second_employee)
